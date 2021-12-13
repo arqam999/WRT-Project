bf58d6ccd112e54f5f7068b2caceda49b2e55b6c
gilagajet <84836081+gilagajet@users.noreply.github.com>
2021-12-13 23:10:15 +0800

    Update lean-openwrt.sh

--- scripts/OpenWRT/lean-openwrt.sh
+++ scripts/OpenWRT/lean-openwrt.sh
@@ -13,8 +13,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' $HWOSDIR/bin/config_generate
 
 
 # Clone community packages to package/community
-mkdir package/community
-pushd package/community
+#mkdir package/community
+#pushd package/community
 
 # Add luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld
@@ -34,9 +34,9 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/wrtbwmon
 rm -rf ../lean/luci-app-wrtbwmon
 
 # Add luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
-rm -rf ../lean/luci-theme-argon
+#git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+#git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+#rm -rf ../lean/luci-theme-argon
 
 #-----------------------------------------------------------------------------
 
@@ -52,9 +52,7 @@ svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentomcat k
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentopd kenzok8/luci-theme-opentopd
 
 #-----------------------------------------------------------------------------
-
-
-popd
+#popd
 
 # Mod zzz-default-settings
 #pushd package/lean/default-settings/files
@@ -80,26 +78,11 @@ sed -i 's/Newifi D2/Mi 4A Gigabit/g' files/etc/banner
 echo -e "          Built on "$(date +%Y.%m.%d)"\n -----------------------------------------------------" >> files/etc/banner
 sed -i "s/OpenWrt /GilaGajet build $(TZ=UTC+8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-
-############REMOVE LEDE CONFIG############
-
-# Add zram-swap
-echo 'CONFIG_PACKAGE_zram-swap=y' >> .config
-echo 'CONFIG_PACKAGE_kmod-zram=y' >> .config
-echo 'CONFIG_PROCD_ZRAM_TMPFS=y' >> .config
-
-
-# Add stunnel
-#echo 'CONFIG_PACKAGE_stunnel=y' >> .config
-
-
-# Add mwan3
-echo 'CONFIG_PACKAGE_luci-app-mwan3=y' >> .config
-echo 'CONFIG_PACKAGE_mwan3=y' >> .config 
+# Enable WiFi Interface
+sed -i 's/wireless.radio${devidx}.disabled=1/wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Modify hostname
 sed -i 's/OpenWrt/Mi4AG/g' package/base-files/files/bin/config_generate
-
 
 # Add kernel build user
 sed -i 's/CONFIG_KERNEL_BUILD_USER=""/CONFIG_KERNEL_BUILD_USER="gilagajet"/g' .config
@@ -113,3 +96,14 @@ sed -i 's/ntp.aliyun.com/time.google.com/g' package/base-files/files/bin/config_
 sed -i 's/time1.cloud.tencent.com/time.cloudflare.com/g' package/base-files/files/bin/config_generate
 sed -i 's/time.ustc.edu.cn/clock.sjc.he.net/g' package/base-files/files/bin/config_generate
 sed -i 's/cn.pool.ntp.org/my.pool.ntp.org/g' package/base-files/files/bin/config_generate
+
+############REMOVE LEDE CONFIG############
+
+# Add zram-swap
+echo 'CONFIG_PACKAGE_zram-swap=y' >> .config
+echo 'CONFIG_PACKAGE_kmod-zram=y' >> .config
+echo 'CONFIG_PROCD_ZRAM_TMPFS=y' >> .config
+
+
+# Add stunnel
+echo 'CONFIG_PACKAGE_stunnel=y' >> .config
