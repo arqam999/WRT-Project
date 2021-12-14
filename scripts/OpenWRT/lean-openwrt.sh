6223d131b98005b0992770c4f55c6837f9afeacb
gilagajet <84836081+gilagajet@users.noreply.github.com>
2021-12-14 23:42:49 +0800

    Update lean-openwrt.sh

--- scripts/OpenWRT/lean-openwrt.sh
+++ scripts/OpenWRT/lean-openwrt.sh
@@ -21,8 +21,8 @@ git clone --depth=1 https://github.com/fw876/helloworld
 
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-sed -i 's/ upx\/host//g' openwrt-passwall/v2ray-plugin/Makefile
-grep -lr upx/host openwrt-passwall/* | xargs -t -I {} sed -i '/upx\/host/d' {}
+#sed -i 's/ upx\/host//g' openwrt-passwall/v2ray-plugin/Makefile
+#grep -lr upx/host openwrt-passwall/* | xargs -t -I {} sed -i '/upx\/host/d' {}
 
 # Add OpenClash
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash
