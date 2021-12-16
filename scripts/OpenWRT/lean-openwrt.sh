035c22bfef2904dea0e5605a882b078aef128f1c
gilagajet <84836081+gilagajet@users.noreply.github.com>
2021-12-16 12:16:40 +0800

    Update lean-openwrt.sh

--- scripts/OpenWRT/lean-openwrt.sh
+++ scripts/OpenWRT/lean-openwrt.sh
@@ -83,17 +83,22 @@ popd
 #popd
 
 # Add luci-app-passwall
-pushd package
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-sed -i 's/ upx\/host//g' openwrt-passwall/v2ray-plugin/Makefile
-grep -lr upx/host openwrt-passwall/* | xargs -t -I {} sed -i '/upx\/host/d' {}
-popd
+#pushd package
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+#sed -i 's/ upx\/host//g' openwrt-passwall/v2ray-plugin/Makefile
+#grep -lr upx/host openwrt-passwall/* | xargs -t -I {} sed -i '/upx\/host/d' {}
+#popd
 
 # Replace latest xray-core
 #rm -r feeds/packages/net/xray-core
 #pushd package
 #svn co https://github.com/xiaorouji/openwrt-passwall/tree/main/xray-core
 #popd
+
+# Add luci-app-passwall from kenzok
+pushd package
+git clone --depth=1 https://github.com/kenzok8/small-package
+popd
 
 # Add OpenClash
 pushd package
