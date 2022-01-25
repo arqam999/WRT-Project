45e4aa9b706c1399b0a77c3e7158bb9208f722d3
gilagajet <84836081+gilagajet@users.noreply.github.com>
2022-01-25 20:28:23 +0800

    Update hook-feeds.sh

--- scripts/Immortal/hook-feeds.sh
+++ scripts/Immortal/hook-feeds.sh
@@ -7,8 +7,8 @@
 
 # Clone Lean's feeds
 mkdir customfeeds
-git clone --depth=1 https://github.com/coolsnowwolf/packages customfeeds/packages
-git clone --depth=1 https://github.com/coolsnowwolf/luci customfeeds/luci
+#git clone --depth=1 https://github.com/coolsnowwolf/packages customfeeds/packages
+#git clone --depth=1 https://github.com/coolsnowwolf/luci customfeeds/luci
 
 # Clone ImmortalWrt's feeds
 pushd customfeeds
