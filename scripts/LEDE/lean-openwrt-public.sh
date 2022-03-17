3511106b7431c0d3bc51ead791a2ad71985a1711
gilagajet <84836081+gilagajet@users.noreply.github.com>
2022-03-17 23:40:06 +0800

    Update lean-openwrt-public.sh

--- scripts/LEDE/lean-openwrt-public.sh
+++ scripts/LEDE/lean-openwrt-public.sh
@@ -25,7 +25,8 @@ mkdir package/community
 pushd package/community
 
 # Add luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+git clone --depth=1 https://github.com/MilesPoupart/openwrt-passwall
 sed -i 's/ upx\/host//g' openwrt-passwall/v2ray-plugin/Makefile
 grep -lr upx/host openwrt-passwall/* | xargs -t -I {} sed -i '/upx\/host/d' {}
 
